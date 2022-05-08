b8d3a823490987d8eafcbc7bf429dcd91fdbfac0
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-08 21:57:03 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -2,42 +2,6 @@ const util = require('./util');
 const axios = require('axios');
 
 module.exports = {
-    getBirthdayData(day, month, year, timezone) {
-        const today = moment().tz(timezone).startOf('day');
-        const wasBorn = moment(`${month}/${day}/${year}`, "MM/DD/YYYY").tz(timezone).startOf('day');
-        const nextBirthday = moment(`${month}/${day}/${today.year()}`, "MM/DD/YYYY").tz(timezone).startOf('day');
-        if (today.isAfter(nextBirthday)) {
-            nextBirthday.add(1, 'years');
-        }
-        const age = today.diff(wasBorn, 'years');
-        const daysAlive = today.diff(wasBorn, 'days');
-        const daysUntilBirthday = nextBirthday.startOf('day').diff(today, 'days'); // same day returns 0
-
-        return {
-            daysAlive: daysAlive, // not used but nice to have :)
-            daysUntilBirthday: daysUntilBirthday,
-            age: age //in years
-        }
-    },
-    createBirthdayReminder(daysUntilBirthday, timezone, locale, message) {
-        moment.locale(locale);
-        const createdMoment = moment().tz(timezone);
-        let triggerMoment = createdMoment.startOf('day').add(daysUntilBirthday, 'days');
-        if (daysUntilBirthday === 0) {
-            triggerMoment = createdMoment.startOf('day').add(1, 'years'); // reminder created on the day of birthday will trigger next year
-        }
-        console.log('Reminder schedule: ' + triggerMoment.format('YYYY-MM-DDTHH:mm:00.000'));
-
-        return util.createReminder(createdMoment, triggerMoment, timezone, locale, message);
-    },
-    getAdjustedDate(timezone) {
-        const today = moment().tz(timezone).startOf('day');
-
-        return {
-            day: today.date(),
-            month: today.month() + 1
-        }
-    },
     fetchRadar(){
         const requests = [];
         for (let i=1; i<=6; i++) {
