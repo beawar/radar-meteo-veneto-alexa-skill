d246d17d3456c6c13535ecaa2b14f7de373f018a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-08 22:17:24 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -17,7 +17,7 @@ module.exports = {
         return Promise.all(requests).then((result) => {
             return result;
         }).catch((error) => {
-            console.error('error on fetching radar images');
+            console.error('error on fetching radar images', error);
             return null;
         });
     }
