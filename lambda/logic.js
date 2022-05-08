0ff866af3b10e6e286e72c6760aa36e52c3d6b9f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-08 22:27:47 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -4,20 +4,26 @@ const axios = require('axios');
 module.exports = {
     fetchRadar(){
         const requests = [];
-        for (let i=1; i<=6; i++) {
-            const url = `https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/teolo/${i}_BASE.jpg`;
-             var config = {
-                timeout: 6500 // timeout api call before we reach Alexa's 8 sec timeout, or set globally via axios.defaults.timeout
-                // headers: {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
-            };
-            requests.push(axios.get(url, config));
-        }
-         
+        // for (let i=1; i<=6; i++) {
+            // const url = `https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/teolo/${i}_BASE.jpg`;
+            //  var config = {
+            //     timeout: 6500 // timeout api call before we reach Alexa's 8 sec timeout, or set globally via axios.defaults.timeout
+            //     // headers: {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
+            // };
+            // requests.push(axios.get(url, config));
+        // }
         
-        return Promise.all(requests).then((result) => {
-            return result;
-        }).catch((error) => {
-            return error;
-        });
+                // return Promise.all(requests).then((result) => {
+        //     return result;
+        // }).catch((error) => {
+        //     return error;
+        // });
+        
+        const url = `https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/teolo/1_BASE.jpg`
+        const config = { timeout: 6500 };
+        return axios.get(url, config)
+        .then((result) => [result])
+        .catch((error) => error);
+        
     }
 }
