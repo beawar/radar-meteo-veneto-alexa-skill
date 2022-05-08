3e559457b8457e2ef472f5933f33b5a92e42002f
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-08 22:11:38 +0000

    updated lambda/logic.js.

--- lambda/logic.js
+++ lambda/logic.js
@@ -4,11 +4,11 @@ const axios = require('axios');
 module.exports = {
     fetchRadar(){
         const requests = [];
-        for (let i=1; i<=6; i++) {
+        for (let i=2; i<=6; i++) {
             const url = `https://www.arpa.veneto.it/bollettini/meteo/radar/imgs/teolo/${i}_BASE.jpg`;
              var config = {
-                timeout: 6500, // timeout api call before we reach Alexa's 8 sec timeout, or set globally via axios.defaults.timeout
-                headers: {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
+                timeout: 6500 // timeout api call before we reach Alexa's 8 sec timeout, or set globally via axios.defaults.timeout
+                // headers: {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8'}
             };
             requests.push(axios.get(url, config));
         }
