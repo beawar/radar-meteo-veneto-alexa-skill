9ffc62a28d062d34a413b915a19764cb5dd9e84c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-09 21:39:10 +0000

    updated lambda/handlers.js.

--- lambda/handlers.js
+++ lambda/handlers.js
@@ -128,34 +128,6 @@ const SessionEndedRequestHandler = {
 /**
  * CUSTOM HANDLERS
  **/
- 
-function showImages(handlerInput, images) {
-    // Add APL directive to response
-    if (util.supportsAPL(handlerInput)) {
-        console.debug('show images', images);
-        const { Viewport } = handlerInput.requestEnvelope.context;
-        const resolution = Viewport.pixelWidth + 'x' + Viewport.pixelHeight;
-        const imagesUrl = images.map((imageSrc) => ({
-            url: imageSrc
-        }));
-        console.debug('images url', imagesUrl);
-        handlerInput.responseBuilder.addDirective({
-            type: 'Alexa.Presentation.APL.RenderDocument',
-            token: constants.APL.radarPlayer.token,
-            document: constants.APL.radarPlayer.document,
-            datasources: {
-                radarImagesData: {
-                    type: 'object',
-                    properties: {
-                        images: imagesUrl
-                    }
-                }
-            }
-        });
-    } else {
-        handlerInput.responseBuilder.speak(handlerInput.t('UNSUPPORTED_MSG', 'Show images is not supported by this device'));
-    }
-}
 
 const ShowRadarIntentHandler = {
     canHandle(handlerInput) {
@@ -183,7 +155,7 @@ const ShowRadarIntentHandler = {
         
         console.log('calling show images');
         
-        showImages(handlerInput, imagesSrc);
+        view.showImages(handlerInput, imagesSrc);
 
         return handlerInput.responseBuilder
             .speak(handlerInput.t('OPEN_PLAYER_MSG', 'Sto aprendo il player'))
