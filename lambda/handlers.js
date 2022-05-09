d5dcdfeda2372febcae0820f62129acf3541ee09
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-09 21:35:26 +0000

    updated lambda/handlers.js.

--- lambda/handlers.js
+++ lambda/handlers.js
@@ -141,25 +141,8 @@ function showImages(handlerInput, images) {
         console.debug('images url', imagesUrl);
         handlerInput.responseBuilder.addDirective({
             type: 'Alexa.Presentation.APL.RenderDocument',
-            version: '1.8',
-            document: constants.APL.radarPlayer,
-            // datasources: {
-            //     launchData: {
-            //         type: 'object',
-            //         properties: {
-            //             headerTitle: handlerInput.t('LAUNCH_HEADER_MSG'),
-            //             mainText: isBirthday ? sessionAttributes['age'] : handlerInput.t('DAYS_LEFT_MSG', {name: '', count: sessionAttributes['daysLeft']}),
-            //             hintString: handlerInput.t('LAUNCH_HINT_MSG'),
-            //             logoImage: isBirthday ? null : Viewport.pixelWidth > 480 ? util.getS3PreSignedUrl('Media/full_icon_512.png') : util.getS3PreSignedUrl('Media/full_icon_108.png'),
-            //             backgroundImage: isBirthday ? util.getS3PreSignedUrl('Media/cake_'+resolution+'.png') : util.getS3PreSignedUrl('Media/papers_'+resolution+'.png'),
-            //             backgroundOpacity: isBirthday ? "1" : "0.5"
-            //         },
-            //         transformers: [{
-            //             inputPath: 'hintString',
-            //             transformer: 'textToHint',
-            //         }]
-            //     }
-            // }
+            token: constants.APL.radarPlayer.token,
+            document: constants.APL.radarPlayer.document,
             datasources: {
                 radarImagesData: {
                     type: 'object',
