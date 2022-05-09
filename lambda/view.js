bb2bd7210e8023696f33c872e5d406d7f76fb40e
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-09 21:38:59 +0000

    updated lambda/view.js.

--- lambda/view.js
+++ lambda/view.js
@@ -15,23 +15,6 @@ function showImages(handlerInput, images) {
             type: 'Alexa.Presentation.APL.RenderDocument',
             token: constants.APL.radarPlayer.token,
             document: constants.APL.radarPlayer.document,
-            // datasources: {
-            //     launchData: {
-            //         type: 'object',
-            //         properties: {
-            //             headerTitle: handlerInput.t('LAUNCH_HEADER_MSG'),
-            //             mainText: isBirthday ? sessionAttributes['age'] : handlerInput.t('DAYS_LEFT_MSG', {name: '', count: sessionAttributes['daysLeft']}),
-            //             hintString: handlerInput.t('LAUNCH_HINT_MSG'),
-            //             logoImage: isBirthday ? null : Viewport.pixelWidth > 480 ? util.getS3PreSignedUrl('Media/full_icon_512.png') : util.getS3PreSignedUrl('Media/full_icon_108.png'),
-            //             backgroundImage: isBirthday ? util.getS3PreSignedUrl('Media/cake_'+resolution+'.png') : util.getS3PreSignedUrl('Media/papers_'+resolution+'.png'),
-            //             backgroundOpacity: isBirthday ? "1" : "0.5"
-            //         },
-            //         transformers: [{
-            //             inputPath: 'hintString',
-            //             transformer: 'textToHint',
-            //         }]
-            //     }
-            // }
             datasources: {
                 radarImagesData: {
                     type: 'object',
