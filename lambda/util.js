22e887f2d7c0957330e9fc34db53b367d92d4d6b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-09 18:45:39 +0000

    updated lambda/util.js.

--- lambda/util.js
+++ lambda/util.js
@@ -1,21 +1,31 @@
+function callDirectiveService(handlerInput, msg) {
+    // Call Alexa Directive Service.
+    const {requestEnvelope} = handlerInput;
+    const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient();
+    const requestId = requestEnvelope.request.requestId;
+    const {apiEndpoint, apiAccessToken} = requestEnvelope.context.System;
+    // build the progressive response directive
+    const directive = {
+        header: {
+            requestId
+        },
+        directive:{
+            type: 'VoicePlayer.Speak',
+            speech: msg
+        }
+    };
+    // send directive
+    return directiveServiceClient.enqueue(directive, apiEndpoint, apiAccessToken);
+}
+
+
+function supportsAPL(handlerInput) {
+    const { supportedInterfaces } = handlerInput.requestEnvelope.context.System.device;
+    return !!supportedInterfaces['Alexa.Presentation.API'];
+}
+
+
 module.exports = {
-    callDirectiveService(handlerInput, msg) {
-        // Call Alexa Directive Service.
-        const {requestEnvelope} = handlerInput;
-        const directiveServiceClient = handlerInput.serviceClientFactory.getDirectiveServiceClient();
-        const requestId = requestEnvelope.request.requestId;
-        const {apiEndpoint, apiAccessToken} = requestEnvelope.context.System;
-        // build the progressive response directive
-        const directive = {
-            header: {
-                requestId
-            },
-            directive:{
-                type: 'VoicePlayer.Speak',
-                speech: msg
-            }
-        };
-        // send directive
-        return directiveServiceClient.enqueue(directive, apiEndpoint, apiAccessToken);
-    }
+    callDirectiveService,
+    supportsAPL
 }
