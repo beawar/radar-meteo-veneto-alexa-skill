17d153e5d159b184c1a8771a21eef2083044a17d
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-10 21:22:10 +0000

    updated lambda/handlers.js.

--- lambda/handlers.js
+++ lambda/handlers.js
@@ -135,26 +135,8 @@ const ShowRadarIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ShowRadarIntent';
     },
     handle(handlerInput) {
-        // try {
-        //     // call the progressive response service
-        //     util.callDirectiveService(handlerInput, handlerInput.t('PROGRESSIVE_MSG'));
-        // } catch (error) {
-        //     // if it fails we can continue, but the user will wait without progressive response
-        //     console.log("Progressive response directive error : " + error);
-        // }
-        // we'll now fetch radar images from an external API
-        // const response = await logic.fetchRadar();
+        // get the src of radar images as an array
         const imagesSrc = logic.fetchRadar();
-        // let speechText = handlerInput.t('API_ERROR_MSG');
-        // if (response && !Array.isArray(response)) {
-        //     speechText = 'Fetch error: ' + response.message;
-        // } else if (response) {
-        //     speechText = handlerInput.t('POSITIVE_SOUND');
-        // }
-        
-        
-        console.log('calling show images');
-        
         view.showImages(handlerInput, imagesSrc);
 
         return handlerInput.responseBuilder
