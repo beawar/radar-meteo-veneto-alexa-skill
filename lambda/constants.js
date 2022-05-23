0d5ce0979e55e8b794d55ab21cc22135827412c1
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2022-05-23 22:02:06 +0000

    updated lambda/constants.js.

--- lambda/constants.js
+++ lambda/constants.js
@@ -10,5 +10,32 @@ module.exports = {
         ENQUEUE: 'ENQUEUE',
         REPLACE_ALL: 'REPLACE_ALL',
         REPLACE_ENQUEUED: 'REPLACE_ENQUEUED'
+    },
+    DetailedAudioWeatherReport: {
+        "type": "AudioPlayer.Play",
+        "audioItem": {
+            "stream": {
+                "url": 'https://www.arpa.veneto.it/previsioni/audio/meteoveneto.mp3',
+                "token": "audioPlayerToken",
+                "offsetInMilliseconds": 0,
+            },
+            "metadata": {
+                "title": "Bollettino dettagliato veneto",
+                "art": {
+                    "sources": [
+                        {
+                            "url": "https://cdn.example.com/url-of-the-album-art-image.png"
+                        }
+                    ]
+                },
+                "backgroundImage": {
+                    "sources": [
+                        {
+                            "url": "https://cdn.example.com/url-of-the-background-image.png"
+                        }
+                    ]
+                }
+            }
+        }
     }
 }
