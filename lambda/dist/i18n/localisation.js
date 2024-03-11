caa7a645b37adc8fbda92ad00275d3ac435aaf10
Beatrice <beatriceguerra.me@gmail.com>
2024-03-11 22:16:40 +0100

    build: auto build

--- lambda/dist/i18n/localisation.js
+++ lambda/dist/i18n/localisation.js
@@ -22,7 +22,7 @@ exports.languageStrings = {
             CANCEL_MSG: `Ok, annullato. `,
             API_ERROR_MSG: `Sto avendo qualche intoppo contattando l'API esterna. Riprova più tardi. `,
             CONJUNCTION_MSG: " e ",
-            HELP_MSG: "Posso recuperare le informazioni del meteo dal sito ArpaV: chiedimi di mostrarti il radar o di leggere il bollettino meteo",
+            HELP_MSG: "Posso recuperare le informazioni del meteo dal sito ARPAV: chiedimi di mostrarti il radar o di leggere il bollettino meteo. Come posso esserti utile?",
             REPORT_GENERAL: "Evoluzione generale",
             REPORT_ALLARM: "Avviso",
             REPORT_PARTICULAR_PHENOMENA: "Fenomeni particolari",
